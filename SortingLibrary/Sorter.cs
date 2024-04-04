3cae93ffa7f124aa64db6a6b64a2d6140d641e1d
Entity-903 <charlesmassie021@gmail.com>
2024-04-04 02:31:23 -0600

    Add project files.

---
+++ SortingLibrary/Sorter.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+using System.Reflection;
+
+namespace SortingLibrary
+{
+	public class Sorter<T> where T : IComparable<T>
+	{
+		/* 
+		 * Arranges numerical elements in a collection from least to greatest
+		Until nothing has changed
+		Iterate over all elements of array except for the last element
+		Compare current element to next element
+			IF current > next
+				swap
+		*/
+		public static void BubbleSort(T[] arr)
+		{
+			bool hasChanged = false;
+			do
+			{
+				hasChanged = false;
+				for (int i = 0; i < arr.Length - 1; i++)
+				{
+					if (arr[i].CompareTo(arr[i + 1]) > 0)
+					{
+						Swap(arr, i, i + 1);
+						hasChanged = true;
+					}
+				}
+			} while (hasChanged);
+		} // O(n^2)
+
+		public static void Swap(T[] a, int indexA, int indexB)
+		{
+			T temp = a[indexA];
+			a[indexA] = a[indexB];
+			a[indexB] = temp;
+		}
+
+		// Arranges numerical elements in a collection from least to greatest
+		public static void InsertionSort(T[] arr)
+		{
+			//int[] numbers = { 5, 2, 3, 1, 4 };
+
+			for (int i = 1; i < arr.Length; i++)
+			{
+				T insertedValue = arr[i];
+				for (int j = 1; j <= i; j++)
+				{
+					if (insertedValue.CompareTo(arr[i - j]) < 0) // insertedValue < arr[i - j]
+					{
+						InsertSwap(arr, i - j, i - j + 1);
+						if (i == j) arr[0] = insertedValue;
+					}
+					else
+					{
+						arr[i - j + 1] = insertedValue;
+						break;
+					}
+				}
+			}
+		} // O(n^2) Due to a for loop within a for loop
+
+		// Copies the value from the old index over to the new index
+		// THIS DOES CHANGE THE VALUE AT indexOld!!!
+		public static void InsertSwap(T[] arr, int indexOld, int indexNew)
+		{
+			arr[indexNew] = arr[indexOld];
+		}
+
+		// Sort from least to greatest
+		public static void SelectionSort(T[] arr)
+		{
+			// [0, 1, 2, 3, 4]
+			//     X
+			for (int i = 0; i < arr.Length; i++)
+			{
+				T selected = arr[i];
+				for (int j = 1; j < arr.Length - i; j++)
+				{
+					// if selected > array at index, swap values
+					if (selected.CompareTo(arr[i + j]) > 0)
+					{
+						SelectionSwap(arr, i + j, ref selected);
+					}
+				}
+				arr[i] = selected;
+			}
+		} // O(n^2) due to a for loop within a for loop
+
+
+		public static void SelectionSwap(T[] arr, int index, ref T selected)
+		{
+			T temp = arr[index];
+			arr[index] = selected;
+			selected = temp;
+		}
+	}
+}
---
+++ TestProject/SorterTests.cs
@@ -0,0 +1,305 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+using SortingLibrary;
+
+namespace SortingLibraryTests
+{
+	[TestClass]
+	public class SorterTests
+	{
+		private int itemsInArray = 100;
+		protected int[] hunRand;
+		protected int[] hunDesc;
+		protected int[] hunAsc;
+		protected string expected;
+
+		#region setupCode
+
+		public SorterTests()
+		{
+			hunRand = new int[itemsInArray];
+			hunDesc = new int[itemsInArray];
+			hunAsc = new int[itemsInArray];
+			Random rand = new Random(12271978);
+			for (int i = 0; i < hunRand.Length; i++)
+			{
+				hunRand[i] = rand.Next(100001);
+			}
+
+			//ten = mil.Take(10).ToArray();
+			hunDesc = hunRand.OrderByDescending(x => x).ToArray();
+			hunAsc = hunRand.OrderBy(x => x).ToArray();
+			expected = ArrayToString(hunAsc);
+		}
+
+		protected int[] CloneRand
+		{
+			get { return (int[])hunRand.Clone(); }
+		}
+
+		protected int[] CloneDesc
+		{
+			get { return (int[])hunDesc.Clone(); }
+		}
+
+		protected int[] CloneAsc
+		{
+			get { return (int[])hunAsc.Clone(); }
+		}
+
+		protected string ArrayToString(int[] a)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			if (a.Length > 0)
+			{
+				sb.Append(a[0]);
+				for (int i = 1; i < a.Length; i++)
+				{
+					sb.Append(", ");
+					sb.Append(a[i]);
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		#endregion
+		/* Psuedo-Code:
+			Test_Sort_HappyPath
+				input = [5, 3, 4, 1, 2]
+				expectedOutput = [1, 2, 3, 4, 5]
+				Assert.IsTrue(sort(input).SequenceEqual(expectedOutput));
+		*/
+		[TestMethod]
+		public void Test_BubbleSort_HappyPath()
+		{
+			int[] numbers = { 5, 2, 3, 1, 4 };
+			int[] expectedNumbers = { 1, 2, 3, 4, 5 };
+			Sorter<int>.BubbleSort(numbers);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+		}
+
+		/* Pseudo-Code:
+			Test_Swap_HappyPath
+				array = [5, 3, 4, 1, 2]
+				indexA = 3
+				indexB = 4
+				expectedOutput = [5, 3, 4, 2, 1]
+				swap(array, indexA, indexB)
+				Assert.IsTrue(SortingLibrary(array).SequenceEqual(expectedOutput))
+		*/
+		[TestMethod]
+		public void Test_Swap_HappyPath()
+		{
+			int[] numbers = { 5, 3, 4, 1, 2 };
+			int indexA = 3;
+			int indexB = 4;
+			int[] expectedNumbers = { 5, 3, 4, 2, 1 };
+			Sorter<int>.Swap(numbers, indexA, indexB);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+		}
+
+		/* Pseudo-Code:
+		[ExpectedException(typeof(NullReferenceException))]
+		Test_Sort_NullCollection
+			input = null
+			sort(input)
+		*/
+		[TestMethod, 
+		ExpectedException(typeof(NullReferenceException))]
+		public void Test_BubbleSort_NullCollection()
+		{
+			int[] input = null;
+			Sorter<int>.BubbleSort(input);
+		}
+
+		/* Pseudo-Code:
+			[ExpectedException(typeof(NullReferenceException))]
+			Test_Swap_NullCollection
+				array = null
+				indexA = null
+				indexB = null
+				swap(array, indexA, indexB)
+		*/
+		[TestMethod,
+		ExpectedException(typeof(NullReferenceException))]
+		public void Test_Swap_NullCollection()
+		{
+			int[] input = null;
+			int indexA = 999;
+			int indexB = 999;
+			Sorter<int>.Swap(input, indexA, indexB);
+		}
+
+		/* Pseudo-Code:
+			[ExpectedException(typeof(IndexOutOfRangeException))]
+			Test_Swap_OutOfBounds
+				array = [1, 6, 3]
+				indexA = 3
+				indexB = 4
+				swap(array, indexA, indexB)
+		*/
+		[TestMethod, 
+		ExpectedException(typeof(IndexOutOfRangeException))]
+		public void Test_Swap_OutOfBounds()
+		{
+			int[] input = { 1, 6, 3 };
+			int indexA = 3;
+			int indexB = 4;
+			Sorter<int>.Swap(input, indexA, indexB);
+		}
+		// Test for the input of a collection with one element
+		[TestMethod]
+		public void OnlyOne_Bubble()
+		{
+			int[] numbers = { 1 };
+			int[] expectedNumbers = { 1 };
+			Sorter<int>.BubbleSort(numbers);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+		}
+//------------------------------------------------
+		[TestMethod]
+		public void Test_InsertionSort_HappyPath()
+		{
+			int[] numbers = { 5, 2, 3, 1, 4 };
+			int[] expectedNumbers = { 1, 2, 3, 4, 5 };
+			Sorter<int>.InsertionSort(numbers);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+		}
+
+		[TestMethod]
+		public void OnlyOne_Insertion()
+		{
+			int[] numbers = { 1 };
+			int[] expectedNumbers = { 1 };
+			Sorter<int>.InsertionSort(numbers);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+		}
+
+		[TestMethod,
+		ExpectedException(typeof(NullReferenceException))]
+		public void Test_InsertionSort_NullCollection()
+		{
+			int[] input = null;
+			Sorter<int>.BubbleSort(input);
+		}
+
+		[TestMethod,
+		ExpectedException(typeof(NullReferenceException))]
+		public void Test_InsertSwap_NullCollection()
+		{
+			int[] input = null;
+			int indexA = 999;
+			int indexB = 999;
+			Sorter<int>.InsertSwap(input, indexA, indexB);
+		}
+
+		[TestMethod,
+		ExpectedException(typeof(IndexOutOfRangeException))]
+		public void Test_InsertSwap_OutOfBounds()
+		{
+			int[] input = { 1, 6, 3 };
+			int indexA = 3;
+			int indexB = 4;
+			Sorter<int>.InsertSwap(input, indexA, indexB);
+		}
+
+		[TestMethod]
+		public void Test_InsertSwap_HappyPath()
+		{
+			int[] numbers = { 5, 3, 4, 1, 2 };
+			int indexA = 3;
+			int indexB = 4;
+			int[] expectedNumbers = { 5, 3, 4, 1, 1 };
+			Sorter<int>.InsertSwap(numbers, indexA, indexB);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+		}
+		//------------------------------------------------
+		[TestMethod]
+		public void Test_SelectionSort_HappyPath()
+		{
+			int[] numbers = { 5, 2, 3, 1, 4 };
+			int[] expectedNumbers = { 1, 2, 3, 4, 5 };
+			Sorter<int>.SelectionSort(numbers);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+		}
+
+		[TestMethod]
+		public void Test_SelectionSwap_HappyPath()
+		{
+			int[] numbers = { 5, 3, 4, 1, 2 };
+			int index = 3;
+			int selected = 4;
+			int[] expectedNumbers = { 5, 3, 4, 4, 2 };
+			Sorter<int>.SelectionSwap(numbers, index, ref selected);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+			Assert.IsTrue(selected == 1);
+		}
+
+		[TestMethod]
+		public void OnlyOne_Selection()
+		{
+			int[] numbers = { 1 };
+			int[] expectedNumbers = { 1 };
+			Sorter<int>.SelectionSort(numbers);
+			Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+		}
+
+		[TestMethod,
+		ExpectedException(typeof(NullReferenceException))]
+		public void Test_SelectionSort_NullCollection()
+		{
+			int[] input = null;
+			Sorter<int>.SelectionSort(input);
+		}
+
+		[TestMethod,
+		ExpectedException(typeof(NullReferenceException))]
+		public void Test_SelectionSwap_NullCollection()
+		{
+			int[] input = null;
+			int index = 999;
+			int selected = 999;
+			Sorter<int>.SelectionSwap(input, index, ref selected);
+		}
+
+		[TestMethod,
+		ExpectedException(typeof(IndexOutOfRangeException))]
+		public void Test_SelectionSwap_OutOfBounds()
+		{
+			int[] input = { 1, 6, 3 };
+			int indexA = 3;
+			int selected = 4;
+			Sorter<int>.SelectionSwap(input, indexA, ref selected);
+		}
+
+
+
+
+	}
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+// Insignificant Testing of Tests
+
+//		[TestMethod]
+//		public void TestEquality()
+//		{
+//			Assert.AreEqual(true, true);
+//		}
+//
+//		[TestMethod]
+//		public void TestRacism()
+//		{
+//			Assert.AreNotEqual(true, false);
+//		}
+//
+//		[TestMethod]
+//		public void TestLowTierExecutable()
+//		{
+//			Assert.Fail("You should exception error yourself NOW!!!");
+//		}
