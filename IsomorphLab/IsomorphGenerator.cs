7c753b3e6aab09a0236c470f0b177f0467e620fe
Entity-903 <charlesmassie021@gmail.com>
2024-04-15 20:56:05 -0600

    Yippie! No errors! Still needs to convert info from dictionaries and list into a very large string.

--- IsomorphLab/IsomorphGenerator.cs
+++ IsomorphLab/IsomorphGenerator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SortingLibrary;
 
 namespace IsomorphLab
 {
@@ -18,10 +19,10 @@ namespace IsomorphLab
 			// Fills the isomorphic dictionaries with every word and respective isomorphic value
 			for (int i = 0; i < words.Count(); i++)
 			{
-				exactIsomorphsDictionary.ContainsKey(words[i]);
-				exactIsomorphsDictionary[words[i]] = CalculateExactIsomorphicValue(words[i]);
-				looseIsomorphsDictionary.ContainsKey(words[i]);
-				looseIsomorphsDictionary[words[i]] = CalculateLooseIsomorphicValue(words[i]);
+				exactIsomorphsDictionary.ContainsKey(words.ElementAt(i));
+				exactIsomorphsDictionary[words.ElementAt(i)] = CalculateExactIsomorphicValue(words.ElementAt(i));
+				looseIsomorphsDictionary.ContainsKey(words.ElementAt(i));
+				looseIsomorphsDictionary[words.ElementAt(i)] = CalculateLooseIsomorphicValue(words.ElementAt(i));
 			}
 			nonIsomorphs = looseIsomorphsDictionary.Keys.Distinct().ToList();
 			for (int i = 0; i < nonIsomorphs.Count(); i++)
@@ -31,14 +32,29 @@ namespace IsomorphLab
 			}
 
 			// display keys with identical isomorphic values
+			string isomorphs = "Exact Isomorphs:\n";
 			foreach (string key in exactIsomorphsDictionary.Keys.ToArray())
 			{
-				string isomorphicValue = exactIsomorphsDictionary[key]
+				string isomorphicValue = exactIsomorphsDictionary[key];
 				if (exactIsomorphsDictionary.ContainsValue(isomorphicValue))
 				{
-
+					// For every unique isomorph
+					// Concatenate to variable isomorphs as follows:
+					// Exact Isomorphs:\n
+					// <isomorphicValue>: <key>, <key>, <key> ... \n
+					// <isomorphicValue>: ... \n
+					// ... \n
+					// \n
+					// Loose Isomorphs:\n
+					// <isomorphicValue>: <key>, <key>, <key> ... \n
+					// <isomorphicValue>: ... \n
+					// ... \n
+					// Non-Isomorphs:\n
+					// <key>, <key>, <key> ...
 				}
 			}
+
+			return isomorphs;
 		}
 
 		public static string CalculateLooseIsomorphicValue(string input)
@@ -58,8 +74,8 @@ namespace IsomorphLab
 			}
 
 			// sorts elements for proper order
-			uint[] conversion = tempDictionary.Values.ToArray();
-			uint[] looseIsomorph = InsertionSort(conversion);
+			uint[] looseIsomorph = tempDictionary.Values.ToArray();
+			Sorter<uint>.InsertionSort(looseIsomorph);
 
 			// converts the array to a string (with spaces)
 			string looseIsomorphicString = "";
@@ -74,34 +90,37 @@ namespace IsomorphLab
 		public static string CalculateExactIsomorphicValue(string input)
 		{
 			Dictionary<string, uint> tempDictionary = new Dictionary<string, uint>();
+			string tempString = "";
 			uint uniqueCharacter = 0;
 			// fills the local dictionary with every unique character along with an associated value
 			// if it encounters a character already in the dictionary, the character adopts that value
 			for (int i = 0; i < input.Length; i++)
 			{
-				if (tempDictionary.ContainsKey(input[i]))
+				if (tempDictionary.ContainsKey(input[i].ToString()))
 				{
 					// character becomes predefined value
-					input[i] = tempDictionary.TryGetValue(input[i]);
+					tempString += tempDictionary[input[i].ToString()];
 				}
 				else
 				{
 					// add character to the dictionary with unique value
-					tempDictionary[input[i]] = uniqueCharacter++;
-		}
+					tempDictionary[input[i].ToString()] = uniqueCharacter++;
+					// character becomes predefined value
+					tempString += tempDictionary[input[i].ToString()];
+				}
 			}
 
-			uint[] exactIsomorph = tempDictionary.;
+			// uint[] exactIsomorph = tempDictionary.ToArray(); // line is incomplete
 			string exactIsomorphicString = "";
-			for (int i = 0; i < exactIsomorph.Length; i++)
+			for (int i = 0; i < tempString.Length; i++)
 			{
-				if (i != exactIsomorph.Length - 1)
+				if (i != tempString.Length - 1)
 				{
-					exactIsomorphicString += exactIsomorph[i] + " ";
+					exactIsomorphicString += tempString[i] + " ";
 				}
 				else
 				{
-					exactIsomorphicString += exactIsomorph[i];
+					exactIsomorphicString += tempString[i];
 				}
 			}
 
